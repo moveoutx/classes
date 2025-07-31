bcbeb5260d1d46d30912471e08c17bc80a2c5295
m2m-relations_ <moveout@yandex.ru>
2025-07-31 15:11:39 +0300

    Раз

---
+++ src/js/app.js
@@ -0,0 +1,87 @@
+import sum from './basic';
+
+export class Character {
+  constructor(name, type) {
+    if (typeof name !== 'string' || name.length < 2 || name.length > 10) {
+      throw new Error('Имя должно быть строкой от 2 до 10 символов');
+    }
+
+    const validTypes = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
+    if (!validTypes.includes(type)) {
+      throw new Error('Неверный тип персонажа');
+    }
+
+    this.name = name;
+    this.type = type;
+    this.health = 100;
+    this.level = 1;
+
+    // Устанавливаем атаку и защиту в зависимости от типа
+    switch (type) {
+      case 'Bowman':
+        this.attack = 25;
+        this.defence = 25;
+        break;
+      case 'Swordsman':
+        this.attack = 40;
+        this.defence = 10;
+        break;
+      case 'Magician':
+        this.attack = 10;
+        this.defence = 40;
+        break;
+      case 'Undead':
+        this.attack = 25;
+        this.defence = 25;
+        break;
+      case 'Zombie':
+        this.attack = 40;
+        this.defence = 10;
+        break;
+      case 'Daemon':
+        this.attack = 10;
+        this.defence = 40;
+        break;
+      default:
+        throw new Error('Неизвестный тип персонажа');
+    }
+  }
+}
+
+export class Bowman extends Character {
+  constructor(name) {
+    super(name, 'Bowman');
+  }
+}
+
+export class Swordsman extends Character {
+  constructor(name) {
+    super(name, 'Swordsman');
+  }
+}
+
+export class Magician extends Character {
+  constructor(name) {
+    super(name, 'Magician');
+  }
+}
+
+export class Daemon extends Character {
+  constructor(name) {
+    super(name, 'Daemon');
+  }
+}
+
+export class Undead extends Character {
+  constructor(name) {
+    super(name, 'Undead');
+  }
+}
+
+export class Zombie extends Character {
+  constructor(name) {
+    super(name, 'Zombie');
+  }
+}
+
+console.log(sum())
